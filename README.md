2b76799641798698f993d80cd98d043a88674964
Minesh Parekh <saixyyu@gmail.com>
2024-02-20 21:03:10 +0000

    finished questions and added forEach for README.md generation

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # README Generator
 
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributions](#contributions)
+* [Tests](#tests)
+* [Questions](#questions)
+* [License](#license)
+
 ## Description
 
-idk
+its a readme generator using node in the command line
+
+## Installation
+
+npm i
+
+## Usage
+
+makes creating a readme file easier.
+
+## Contributions
+
+None
+
+## Tests
+
+None
+
+## Questions
+
+ If you have any questions, contact us here: 
+
+* [GitHub Profile](https://github.com/minyyds)
+* Email: minesh651@gmail.com
+
+## License
+
+MIT
 
--- index.js
+++ index.js
@@ -42,6 +42,28 @@ const questions = [
     message: "Give a brief description of your project :",
 },
 {
+    type: "input",
+    name: "installation",
+    message: "What is the installation process for your project? :",
+    default: "npm i"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What is the use of your project? :"
+},
+{
+    type: "input",
+    name: "contributions",
+    message: "What are the contributions for this project? :"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "If command should be ran to run tests? :",
+    default: "npm test"
+},
+{
     type: "list",
     name: "license",
     message: "Choose a license for this project :",
@@ -61,19 +83,54 @@ const questions = [
         "Mozilla General V2.0",
         "The Unlicense"
     ]
-},
+}
 
 ]
 
 inquirer.prompt(questions).then((answers) => {
 const readmeContent = generateReadme(answers)
-fs.writeFile('README.md', readmeContent, (err) => {
+fs.writeFile("README.md", readmeContent, (err) => {
     if (err) throw err
-    console.log('README.md has been created!')
+    console.log("README.md has been created!")
 })
 })
 
 function generateReadme(answers) {
-return `# ${answers.title}\n\n## Description\n\n${answers.description}\n\n`
-// ${}\n\n
+    const sections = [
+        "Description",
+        "Installation",
+        "Usage",
+        "Contributions",
+        "Tests",
+        "Questions",
+        "License"
+    ];
+
+    let readmeContent = `# ${answers.title}\n\n`;
+
+    readmeContent += `## Table of Contents\n\n`;
+    sections.forEach(section => {
+        readmeContent += `* [${section}](#${section.toLowerCase().replace(/ /g, '-')})\n`;
+    });
+    readmeContent += `\n`;
+
+    sections.forEach(section => {
+        if (section === "Tests") {
+            readmeContent += `## ${section}\n\n If you wish to run tests just input: \n\n`
+        }
+        if (section === "Questions") {
+            readmeContent += `## ${section}\n\n If you have any questions, contact me here: \n\n`;
+            if (answers.username) {
+                readmeContent += `* [GitHub Profile](https://github.com/${answers.username})\n`;
+            }
+            if (answers.email) {
+                readmeContent += `* Email: ${answers.email}\n`;
+            }
+            readmeContent += `\n`;
+        } else if (answers[section.toLowerCase()]) {
+            readmeContent += `## ${section}\n\n${answers[section.toLowerCase()]}\n\n`;
+        }
+    });
+
+    return readmeContent;
 }
